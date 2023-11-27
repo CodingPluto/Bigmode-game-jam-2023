3e813429d015e2e70224e222a5d11a028d28cccb
CodingPluto <95600256+CodingPluto@users.noreply.github.com>
2023-11-27 14:21:45 +1300

    CUSTOM_COPY_COMMAND set up for copying dlls to bin folder upon build. SDL2 has been initalised in program, main loop not yet set up.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <my_header.h>
 #include <SDL.h>
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
   std::cout << "Here is my Test Program with a number of: " << a << std::endl;
 
-  SDL_Init(SDL_INIT_EVERYTHING);
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    std::cout << "SDL not initalised properly. Error: \n" << SDL_GetError() << std::endl;
+    throw "SDL not initalised, quitting program.";
+  }
 
+
+  SDL_Window* win = SDL_CreateWindow("Test", 0, 0, 500, 500, 0);
+  SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+  bool game_running = true;
+  while (game_running)
+  {
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+
+    }
+  }
 
   return 0;
 }
