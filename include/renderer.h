fd8378a8bb20c8ada89d75ec0ba8d26f67702130
Cerberus <hugh.bill@icloud.com>
2023-11-27 16:16:04 +1300

    finished renderer.h file

--- include/renderer.h
+++ include/renderer.h
@@ -4,17 +4,19 @@
 #include <iostream>
 #include <vector>
 #include <SDL.h>
+#include <visual_data.h>
 
 Class Renderer{
 private:
-	std::vector<image_data> render_list_;
-	SDL_Renderer* sdl2_rend;
-
-	void Render() {}
+	std::vector<visual_data*> render_list_;
+	SDL_Renderer* sdl2_rend_;
+	SDL_Window* win_;
+	void Render();
 
 public:
-	Renderer() {};
-
+	Renderer(SDL_Renderer* sdl2_rend, SDL_Window* win);
+	int AddToRenderList(visual_data* obj_data, int z_order);
+	void RemoveFromRenderList(int ID);
 }
 
 
---
+++ src/renderer.cpp
@@ -0,0 +1,14 @@
+#include "renderer.h"
+
+void Render()
+{
+}
+
+int AddToRenderList(visual_data* obj_data, int z_order)
+{
+	return 0;
+}
+
+void RemoveFromRenderList(int ID)
+{
+}
