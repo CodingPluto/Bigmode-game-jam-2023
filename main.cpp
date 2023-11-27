bb3144592b0d1cc607d04725de95900c3c3349d0
CodingPluto <95600256+CodingPluto@users.noreply.github.com>
2023-11-27 13:58:12 +1300

    inital commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <my_header.h>
+#include <SDL.h>
+int main(int argc, char *argv[])
+{
+  std::cout << "Here is my Test Program with a number of: " << a << std::endl;
+
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+
+  return 0;
+}
---
+++ my_header.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+extern int a = 5;
+
+
+#endif // MY_HEADER_H
