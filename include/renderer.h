bf8bfdd9d802c184022c86b1d3c7768ab01c8e86
Cerberus <hugh.bill@icloud.com>
2023-11-27 15:52:33 +1300

    work on renderer

--- include/renderer.h
+++ include/renderer.h
@@ -3,10 +3,18 @@
 
 #include <iostream>
 #include <vector>
+#include <SDL.h>
 
 Class Renderer{
 private:
-	std::vector<image_data> _render_list;
+	std::vector<image_data> render_list_;
+	SDL_Renderer* sdl2_rend;
+
+	void Render() {}
+
+public:
+	Renderer() {};
+
 }
 
 
