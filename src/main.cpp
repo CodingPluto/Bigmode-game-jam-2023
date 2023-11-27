e399b233e2cdce085b3d02bfa238c2cc817948d9
CodingPluto <95600256+CodingPluto@users.noreply.github.com>
2023-11-27 16:57:56 +1300

    Added new libraries. They haven't been linked yet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
 #include <my_header.h>
 #include <SDL.h>
+#include <SDL_image.h>
 int main(int argc, char *argv[]){
   std::cout << "Here is my Test Program with a number of: " << a << std::endl;
 
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     std::cout << "SDL not initalised properly. Error: \n" << SDL_GetError() << std::endl;
-    //throw "SDL not initalised, quitting program.";
   }
   
-
+  
   SDL_Window* win = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, 0);
   SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
   bool run = true;
-  while (run)
-  {
+  while (run){
     SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-      if (event.type == SDL_QUIT)
-      {
-          run = false;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        run = false;
       }
     }
   }
-
   return 0;
 }
 
